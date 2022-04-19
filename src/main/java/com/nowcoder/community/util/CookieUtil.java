ffc5063a16e13363f105e82e29303c47350c4f3c
xiaomeng <xiaomengcd18@163.com>
2022-04-19 10:44:59 +0800

    code loginTicketInterceptor

---
+++ src/main/java/com/nowcoder/community/util/CookieUtil.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author : zhiHao
+ * @since : 2022/4/19
+ */
+public class CookieUtil {
+
+    public static String getValue(HttpServletRequest request, String name) {
+        if (request == null || name == null) {
+            throw new IllegalArgumentException("参数为空");
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
